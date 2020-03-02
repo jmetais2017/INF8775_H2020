c477ba63f59d86c48b7466ebde3ab4c795d0f8b2
DESKTOP-JP5PSSI\Simon Ritchot <simon.ritchot@polymtl.ca>
2020-03-01 21:30:52 -0500

    rapport avancement

--- TP2/glouton.py
+++ TP2/glouton.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 
+
 def glouton(C, notes):
     start = time.time()
 
@@ -9,29 +10,32 @@ def glouton(C, notes):
     totalCost = 0
     solution = []
 
-
-    #Initialisation : choix de d0 et d1
+    # Initialisation : choix de d0 et d1
     transitions = [C[notes[0], :, notes[1], :]]
-    bestCost = np.argmin(transitions) #Récupréation de la transition permettant de passer de la première à la deuxième note ayant le coût le plus faible
+
+    # print(transitions)
+
+    # Récupréation de la transition permettant de passer de la première à la deuxième note ayant le coût le plus faible
+    bestCost = np.argmin(transitions)
+    # print(bestCost)
     bestd0 = int(bestCost / 5)
-    bestd1 = bestCost - 5*bestd0
+    bestd1 = bestCost - 5 * bestd0
     bestTransition = transitions[0][bestd0][bestd1]
 
     solution.append(bestd0)
     solution.append(bestd1)
     totalCost += bestTransition
 
-
-    #Remplissage de la solution
+    # Remplissage de la solution
     for currentId in range(2, exSize):
-        bestFinger = np.argmin(C[notes[currentId - 1]][solution[currentId - 1]][notes[currentId]])
-        bestTransition = C[notes[currentId - 1]][solution[currentId - 1]][notes[currentId]][bestFinger]
+        bestFinger = np.argmin(C[notes[currentId - 1]]
+                               [solution[currentId - 1]][notes[currentId]])
+        bestTransition = C[notes[currentId - 1]
+                           ][solution[currentId - 1]][notes[currentId]][bestFinger]
 
         solution.append(bestFinger)
         totalCost += bestTransition
 
-
     end = time.time()
 
-    return end-start, totalCost, solution
-
+    return end - start, totalCost, solution
--- TP2/main.py
+++ TP2/main.py
@@ -14,4 +14,6 @@ cout_transition = load_file.reshape((24, 5, 24, 5))
 notes = np.loadtxt("./Chansons/fur_elise.txt", skiprows = 1, dtype=int)
 
 gloutonTime, gloutonCost, gloutonSol = glouton(cout_transition, notes)
-print(localSearch(cout_transition, notes, gloutonSol, 100))
+
+
+#print(localSearch(cout_transition, notes, gloutonSol, 100))
